27dcf573f3d6a14bd71576b853e3a3931d167991
Unknown <colin.jerome29@gmail.com>
2017-03-01 12:01:43 +0100

    Gameplay fonctionnel
    Gameplay fonctionnel

--- Assets/MoveEnemy.cs
+++ Assets/MoveEnemy.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class MoveEnemy : MonoBehaviour {
 	[HideInInspector]
@@ -8,9 +7,9 @@ public class MoveEnemy : MonoBehaviour {
 	private int currentWaypoint = 0;
 	private float lastWaypointSwitchTime;
 	public float speed = 1.0f;
+
 	// Use this for initialization
 	void Start () {
-	
 		lastWaypointSwitchTime = Time.time;
 	}
 	
@@ -26,34 +25,52 @@ public class MoveEnemy : MonoBehaviour {
 		gameObject.transform.position = Vector3.Lerp (startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
 		// 3 
 		if (gameObject.transform.position.Equals(endPosition)) {
-  			if (currentWaypoint < waypoints.Length - 2) {
-    // 3.a 
-    			currentWaypoint++;
-  				lastWaypointSwitchTime = Time.time;
-    			RotateIntoMoveDirection();
- 			 } else {
-    // 3.b 
-    			Destroy(gameObject);
+			if (currentWaypoint < waypoints.Length - 2) {
+				// 4 Switch to next waypoint
+				currentWaypoint++;
+				lastWaypointSwitchTime = Time.time;
+			
+				RotateIntoMoveDirection();
+			} else {
+				// 5 Destroy enemy
+				Destroy(gameObject);
  
-    			AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-    			AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
-    // TODO: deduct health
-  			}
+				AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+				AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+
+				GameManagerBehavior gameManager =
+					GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+				gameManager.Health -= 1;
+			}
 		}
 	}
+
 	private void RotateIntoMoveDirection() {
-  		//1
-  		Vector3 newStartPosition = waypoints [currentWaypoint].transform.position;
-  		Vector3 newEndPosition = waypoints [currentWaypoint + 1].transform.position;
-  		Vector3 newDirection = (newEndPosition - newStartPosition);
-  		//2
-  		float x = newDirection.x;
-  		float y = newDirection.y;
-  		float rotationAngle = Mathf.Atan2 (y, x) * 180 / Mathf.PI;
-  		//3
-  		GameObject sprite = (GameObject)
-    	  gameObject.transform.FindChild("Sprite").gameObject;
-  		sprite.transform.rotation = 
-    		Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+		//1
+		Vector3 newStartPosition = waypoints [currentWaypoint].transform.position;
+		Vector3 newEndPosition = waypoints [currentWaypoint + 1].transform.position;
+		Vector3 newDirection = (newEndPosition - newStartPosition);
+		//2
+		float x = newDirection.x;
+		float y = newDirection.y;
+		float rotationAngle = Mathf.Atan2 (y, x) * 180 / Mathf.PI;
+		//3
+		GameObject sprite = (GameObject)
+			gameObject.transform.FindChild("Sprite").gameObject;
+		sprite.transform.rotation = 
+			Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+	}
+
+	public float distanceToGoal() {
+		float distance = 0;
+		distance += Vector3.Distance(
+			gameObject.transform.position, 
+			waypoints [currentWaypoint + 1].transform.position);
+		for (int i = currentWaypoint + 1; i < waypoints.Length - 1; i++) {
+			Vector3 startPosition = waypoints [i].transform.position;
+			Vector3 endPosition = waypoints [i + 1].transform.position;
+			distance += Vector3.Distance(startPosition, endPosition);
+		}
+		return distance;
 	}
 }
